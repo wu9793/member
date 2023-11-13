eb4d8deca2211fb84659d5b27d2b903b50bf9fa8
wu <timmy61501@gmail.com>
2023-11-13 13:50:25 +0800

    表單資料檢查

--- add_user.php
+++ add_user.php
@@ -2,9 +2,11 @@
 
 $dsn="mysql:host=localhost;charset=utf8;dbname=member";
 $pdo=new PDO($dsn,'root','');
+// 資料檢查
+$acc=htmlspecialchars(trim($_POST['acc']));
 
 $sql="insert into `users`(`acc`,`pw`,`name`,`email`,`address`)
-values('{$_POST['acc']}','{$_POST['pw']}','{$_POST['name']}','{$_POST['email']}','{$_POST['address']}')";
+values('{$acc}','{$_POST['pw']}','{$_POST['name']}','{$_POST['email']}','{$_POST['address']}')";
 $pdo->exec($sql);
 
 // 使用者key完資料後導向主頁
